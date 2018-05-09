6c124f83946ca2cbb61e467995cda32661b3a079
insectman <insectman@yandex.ru>
2018-05-10 00:54:58 +0300

    fix CORS issue

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 app.post('/', async (req, res) => {
   try {
     const fetch = require('node-fetch');
